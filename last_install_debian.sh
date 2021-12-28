d753210fcc6d0dca65c2cf019692d4e386b8008b
Erik Ccanto <erik.ccanto@eclypsium.com>
2021-12-28 19:35:32 -0300

    shell

--- last_install_debian.sh
+++ last_install_debian.sh
@@ -12,10 +12,25 @@ DEPENDENCIES=./.deps
 POWERLEVEL10K_DIR=${DEPENDENCIES}/powerlevel10k
 
 # powerlevel10k
+if ! command -v zsh &> /dev/null; then
+    sudo apt update
+    sudo apt install zsh
+fi
+
 if [[ ! -d ${POWERLEVEL10K_DIR} ]]; then
     echo -e "${BOLDGREEN}installing powerlevel10k...${ENDCOLOR}"
 
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${POWERLEVEL10K_DIR}
-    echo "source ${POWERLEVEL10K_DIR}/powerlevel10k.zsh-theme" >>~/.zshrc
+    sudo rm -rf /usr/local/share/powerlevel10k/
+    sudo cp -r ${POWERLEVEL10K_DIR} /usr/local/share/powerlevel10k
+    echo "source /usr/local/share/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
+
     zsh
+    sudo su -c 'zsh'  # configure powerlevel10k in the root user.
+
+    sed -i 's/^echo "Welcome to Parrot OS"$/#echo "Welcome to Parrot OS"/g' ~/.zshrc
+    sudo ln -s -f $(realpath ~/.zshrc) /root/.zshrc
+
+    sudo usermod --shell /usr/bin/zsh $(whoami)
+    sudo usermod --shell /usr/bin/zsh root
 fi
