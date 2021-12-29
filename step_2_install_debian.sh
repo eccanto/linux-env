986c166dcdeec766daeb226a0e0cd452809ad5aa
Erik Ccanto <erik.ccanto@eclypsium.com>
2021-12-29 01:02:38 -0300

    refactor and wallpapers

---
+++ step_2_install_debian.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source common.sh
+
+# Hack Nerd Fonts
+if ! ls /usr/local/share/fonts/Hack*.ttf &> /dev/null; then
+    echo -e "${BOLDGREEN}installing Hack Nerd Fonts...${ENDCOLOR}"
+
+    mkdir -p ${FONTS_DIR}
+    pushd ${FONTS_DIR}
+        wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
+        sudo mv Hack.zip /usr/local/share/fonts/
+        cd /usr/local/share/fonts/
+        sudo unzip Hack.zip
+        sudo rm Hack.zip
+    popd
+    fc-cache -v
+fi
+
+# firefox latest
+if ! command -v firefox &> /dev/null; then  # TODO -> verify version!!!
+    echo -e "${BOLDGREEN}installing firefox...${ENDCOLOR}"
+    wget -O ~/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64"
+    sudo tar xjf ~/FirefoxSetup.tar.bz2 -C /opt/
+    rm ~/FirefoxSetup.tar.bz2
+    sudo mv /usr/bin/firefox /usr/bin/firefox_backup
+    sudo ln -s /opt/firefox/firefox /usr/bin/firefox
+fi
+
+# firejail
+if ! command -v firejail &> /dev/null; then
+    echo -e "${BOLDGREEN}installing firejail...${ENDCOLOR}"
+    sudo apt install firejail
+fi
+
+# feh
+if ! command -v feh &> /dev/null; then
+    echo -e "${BOLDGREEN}installing wallpapers...${ENDCOLOR}"
+    sudo apt install feh
+    sudo mkdir -p ${WALLPAPERS_STORAGE}
+    sudo cp ${DEFAULT_BG} ${WALLPAPERS_STORAGE}
+
+    echo -e "\n# background\nfeh --bg-fill ${WALLPAPERS_STORAGE}/$(basename ${DEFAULT_BG})" >> ${BSPWM_CONFIG}/bspwmrc
+    pkill -USR1 -x sxhkd || true
+fi
+
+# rofi
+if ! command -v rofi &> /dev/null; then
+    sudo apt install rofi
+fi
+
+if [[ ! -d ${ROFI_CONFIG} ]]; then
+    echo -e "${BOLDGREEN}configuring rofi...${ENDCOLOR}"
+
+    mkdir -p ${ROFI_CONFIG}/themes
+    cp ./rofi/nord.rasi ${ROFI_CONFIG}/themes
+fi
+
+echo -e "select nord theme and press ${BOLDGREEN}'Alt + a'${ENDCOLOR}"
+
+rofi-theme-selector
+
+# slim and slimlock
+if ! command -v slimlock &> /dev/null; then
+    sudo apt update
+    sudo apt install slim libpam0g-dev libxrandr-dev libfreetype6-dev libimlib2-dev libxft-dev
+
+    git clone https://github.com/joelburget/slimlock.git ${SLIMLOCK_DIR}
+    pushd ${SLIMLOCK_DIR}
+        sudo make
+        sudo make install
+    popd
+
+    pushd ./slim/
+        sudo cp slim.conf /etc/
+        sudo cp slimlock.conf /etc/
+        sudo cp -r default /usr/share/slim/themes/
+    popd
+
+    echo -e "\n# Slimlock\nsuper + l\n    slimlock" >> ${SXHKD_CONFIG}/sxhkdrc
+fi
+
+# end
+echo -e "${BOLDGREEN}\nfinished.${ENDCOLOR}\n"
+echo -e "you must reboot the machine and then run the ${BOLDGREEN}\"bash last_install_debian.sh\"${ENDCOLOR} command to complete the installation."
---
+++ step_3_install_debian.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source common.sh
+
+# powerlevel10k
+if ! command -v zsh &> /dev/null; then
+    echo -e "${BOLDGREEN}installing zsh...${ENDCOLOR}"
+
+    sudo apt update
+    sudo apt install zsh
+fi
+
+if [[ ! -d ${POWERLEVEL10K_DIR} ]]; then
+    echo -e "${BOLDGREEN}installing powerlevel10k...${ENDCOLOR}"
+
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${POWERLEVEL10K_DIR}
+    sudo rm -rf /usr/local/share/powerlevel10k/
+    sudo cp -r ${POWERLEVEL10K_DIR} /usr/local/share/powerlevel10k
+    echo "source /usr/local/share/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
+
+    zsh
+    sudo su -c 'zsh'  # configure powerlevel10k in the root user.
+
+    sed -i 's/^echo "Welcome to Parrot OS"$/#echo "Welcome to Parrot OS"/g' ~/.zshrc
+    sudo ln -s -f $(realpath ~/.zshrc) /root/.zshrc
+
+    sudo usermod --shell /usr/bin/zsh $(whoami)
+    sudo usermod --shell /usr/bin/zsh root
+
+    chown $(whoami):$(whoami) /root
+    chown $(whoami):$(whoami) /root/.cache -R
+    chown $(whoami):$(whoami) /root/.local -R
+fi
+
+# bat
+if ! command -v bat &> /dev/null; then
+    echo -e "${BOLDGREEN}installing bat...${ENDCOLOR}"
+
+    wget https://github.com/sharkdp/bat/releases/download/v0.18.3/bat_0.18.3_amd64.deb -P ${DEPENDENCIES}
+    sudo dpkg -i ${DEPENDENCIES}/bat_0.18.3_amd64.deb
+
+    echo "alias cat='/bin/bat'" >> ~/.zshrc
+    echo "alias catn='/bin/cat'" >> ~/.zshrc
+    echo "alias catnl='/bin/bat --paging=never'" >> ~/.zshrc
+fi
+
+# lsd
+if ! command -v lsd &> /dev/null; then
+    echo -e "${BOLDGREEN}installing lsd...${ENDCOLOR}"
+
+    wget https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_amd64.deb -P ${DEPENDENCIES}
+    sudo dpkg -i ${DEPENDENCIES}/lsd_0.20.1_amd64.deb
+
+    echo "alias ll='lsd -lh --group-dirs=first'" >> ~/.zshrc
+    echo "alias la='lsd -a --group-dirs=first'" >> ~/.zshrc
+    echo "alias l='lsd --group-dirs=first'" >> ~/.zshrc
+    echo "alias lla='lsd -lha --group-dirs=first'" >> ~/.zshrc
+    echo "alias ls='lsd --group-dirs=first'" >> ~/.zshrc
+fi
+
+# fzf
+if ! command -v fzf &> /dev/null; then
+    echo -e "${BOLDGREEN}installing fzf...${ENDCOLOR}"
+
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+fi
+
+# ranger
+if ! command -v ranger &> /dev/null; then
+    echo -e "${BOLDGREEN}installing ranger...${ENDCOLOR}"
+
+    sudo apt install ranger
+fi
+
+# zsh pluggins
+echo -e "${BOLDGREEN}installing zsh plugin: sudo.plugin.zsh...${ENDCOLOR}"
+
+sudo mkdir -p /usr/share/zsh-plugins/
+sudo chown $(whoami):$(whoami) /usr/share/zsh-plugins/
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh -P /usr/share/zsh-plugins/
+
+echo "source /usr/share/zsh-plugins/sudo.plugin.zsh" >> ~/.zshrc
+
+# nvim
+if ! command -v nvim &> /dev/null; then
+    echo -e "${BOLDGREEN}installing neovim...${ENDCOLOR}"
+    wget https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-linux64.tar.gz -O- | sudo tar zxvf - -C /usr/local --strip=1
+
+    sudo mv /usr/bin/vim /usr/bin/vim_backup
+    sudo ln -s /usr/local/bin/nvim /usr/bin/vim
+fi
+
+rm ${NVIM_CONFIG}/init.vim
+echo -e "${BOLDGREEN}installing neovim style...${ENDCOLOR}"
+
+mkdir -p ${NVIM_CONFIG}
+
+cat > ${NVIM_CONFIG}/init.vim << EOC
+set number
+
+set expandtab
+set autoindent
+set softtabstop=4
+set shiftwidth=4
+set tabstop=4
+
+"Enable mouse click for nvim
+set mouse=a
+"Fix cursor replacement after closing nvim
+set guicursor=
+"Shift + Tab does inverse tab
+inoremap <S-Tab> <C-d>
+
+"See invisible characters
+set list listchars=tab:>\ ,trail:.
+
+"wrap to next line when end of line is reached
+set whichwrap+=<,>,[,]
+
+syntax on
+EOC
+
+# system
+echo -e "${BOLDGREEN}updating system files...${ENDCOLOR}"
+sudo updatedb
