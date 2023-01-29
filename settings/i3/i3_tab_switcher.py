7ac7e8147ddb63b50767bb4ba3cca19ebe1da87a
Erik Ccanto <ccanto.erik@gmail.com>
2023-01-28 20:47:40 -0500

    fix: don't switch between workspaces - i3_tab_switcher

--- settings/i3/i3_tab_switcher.py
+++ settings/i3/i3_tab_switcher.py
@@ -2,7 +2,7 @@
 from i3ipc import Connection
 
 
-def focus_first(i3, node):
+def focus_first(i3, node) -> None:
     if node.type != 'workspace':
         while len(node.focus) > 1:
             node = i3.get_tree().find_by_id(node.focus[0])
@@ -16,24 +16,25 @@ def focus_next(focused=None) -> None:
     if focused is None:
         focused = i3.get_tree().find_focused()
 
-    if len(focused.parent.nodes) > 1:
-        parent_nodes = [node.id for node in focused.parent.nodes]
-        parent_nodes_length = len(parent_nodes)
-        position = parent_nodes.index(focused.id)
+    if focused.type != 'workspace':
+        if len(focused.parent.nodes) > 1:
+            parent_nodes = [node.id for node in focused.parent.nodes]
+            parent_nodes_length = len(parent_nodes)
+            position = parent_nodes.index(focused.id)
 
-        if parent_nodes_length > 1:
-            if position < parent_nodes_length - 1:
-                i3.command(f'[con_id={parent_nodes[position + 1]}] focus')
-            else:
-                if len(focused.nodes) > 1:
-                    focus_first(i3, focused)
+            if parent_nodes_length > 1:
+                if position < parent_nodes_length - 1:
+                    i3.command(f'[con_id={parent_nodes[position + 1]}] focus')
                 else:
-                    focus_first(i3, i3.get_tree().find_by_id(parent_nodes[0]))
+                    if len(focused.nodes) > 1:
+                        focus_first(i3, focused)
+                    else:
+                        focus_first(i3, i3.get_tree().find_by_id(parent_nodes[0]))
+            else:
+                i3.command('focus parent')
+                focus_next()
         else:
-            i3.command('focus parent')
-            focus_next()
-    else:
-        focus_next(focused.parent)
+            focus_next(focused.parent)
 
 
 if __name__ == '__main__':
