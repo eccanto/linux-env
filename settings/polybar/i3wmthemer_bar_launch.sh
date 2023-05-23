fdfec31d3e5f3bfbd9af8119b5ebeab46e6fd33f
eccanto <ccanto.erik@gmail.com>
2023-05-22 22:57:00 -0300

    feat!: set HDMI as primary monitor if is connected

--- settings/polybar/i3wmthemer_bar_launch.sh
+++ settings/polybar/i3wmthemer_bar_launch.sh
@@ -4,6 +4,12 @@ pkill polybar
 
 sleep 1;
 
-polybar i3wmthemer_bar &
+# set HDMI as primary monitor if is connected
+MONITOR="$(xrandr -q | grep ' connected ' | grep HDMI | cut -d' ' -f1 | head -1)"
+if [[ -z "${MONITOR}" ]]; then
+    MONITOR="$(xrandr -q | grep ' connected ' | cut -d' ' -f1 | head -1)"
+fi
+
+MONITOR=${MONITOR} polybar i3wmthemer_bar &
 picom --experimental-backends -b
 feh --bg-fill /home/eccanto/.wallpaper.jpg
