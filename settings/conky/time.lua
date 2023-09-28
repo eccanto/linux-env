5dd8d89dcab85f48e0c094e3b25400cf5f5e346b
Erik Ccanto <ccanto.erik@gmail.com>
2023-09-27 22:43:16 -0300

    refactor: change conky style

--- settings/conky/time.lua
+++ settings/conky/time.lua
@@ -6,7 +6,7 @@ conky.config = {
   imlib_cache_size = 10,
   own_window = true,
   own_window_class = 'Conky',
-  own_window_transparent = true,
+  own_window_transparent = false,
   own_window_argb_visual = false,
   own_window_argb_value = 100,
   own_window_colour = '000000',
@@ -15,15 +15,15 @@ conky.config = {
   override_utf8_locale = true,
   use_xft = true,
   uppercase = false,
-  minimum_width = 210,
+  minimum_width = 333,
   alignment = 'top_right',
   gap_x = 22,
   gap_y = 22
 };
 
 conky.text = [[
-${voffset 0}${offset 0}${font Noto:size=34}${color white}${time %e}
-${goto 20}${font Noto:size=18}${color white}${voffset -16}${time %b}${color white}${offset 10}${time %Y}
+${voffset 0}${offset 12}${font Noto:size=34}${color white}${time %e}
+${goto 19}${font Noto:size=18}${color white}${voffset -16}${time %b}${color white}${offset 10}${time %Y}
 ${font Noto:size=12}${color white}${voffset 5}\
 ${goto 20}${time %A}${goto 153}${color white}${time %H}:${time %M}
 ]];
