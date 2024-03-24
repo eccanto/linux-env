3eee0ac21161357f6b369305d5ac6ffa5b13c250
eccanto <ccanto.erik@gmail.com>
2024-03-24 18:52:08 -0300

    fix: restore i3lock-color

--- tools/i3lock/install.sh
+++ tools/i3lock/install.sh
@@ -13,17 +13,17 @@ if [[ ${os_name} == Ubuntu* ]]; then
     sudo apt install --assume-yes autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev \
         libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev \
         libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
-
-    git clone --depth=1 https://github.com/Raymo111/i3lock-color.git "${TEMPORARY_DIRECORY}"
-
-    pushd "${TEMPORARY_DIRECORY}"
-        ./build.sh
-        ./install-i3lock-color.sh
-    popd
-
-    rm -rf "${TEMPORARY_DIRECORY}"
 elif [[ ${os_name} == Manjaro* ]]; then
     sudo pacman -S i3lock
 fi
+
+git clone --depth=1 https://github.com/Raymo111/i3lock-color.git "${TEMPORARY_DIRECORY}"
+
+pushd "${TEMPORARY_DIRECORY}"
+    ./build.sh
+    ./install-i3lock-color.sh
+popd
+
+rm -rf "${TEMPORARY_DIRECORY}"
 
 sudo cp "${I3LOCK_EXECUTABLE}" /usr/bin/lock
