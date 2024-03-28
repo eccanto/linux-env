e3d387c7912aa0ec50779f3bee88c358115f4361
eccanto <ccanto.erik@gmail.com>
2024-03-28 10:52:14 -0300

    refactor: add xdotool to arch installation

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ function install_system_requirements_debian() {
 function install_system_requirements_arch() {
     sudo pacman -Sy
     sudo pacman -S python-pip python-pipx python3-i3ipc git zip unzip curl feh flameshot arandr htop peek gucharmap \
-                   nodejs-lts-iron npm lua keychain pyenv noto-fonts-emoji
+                   nodejs-lts-iron npm lua keychain pyenv noto-fonts-emoji xdotool
 
     if ! command -v rustc &> /dev/null; then
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
