115b128ec194a7848a9edc0b906555d9b9946b38
eccanto <ccanto.erik@gmail.com>
2024-03-29 20:02:30 -0300

    refactor: add installation script for pyenv on Arch

--- tools/pyenv/install.sh
+++ tools/pyenv/install.sh
@@ -1,1 +1,14 @@
-sudo apt install libssl-dev python3-tk libreadline-dev libsqlite3-dev tk-dev
+#!/usr/bin/env bash
+
+set -xeu
+
+SCRIPT_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+
+source "${SCRIPT_PATH}/../../common.sh"
+
+os_name=$(get_os)
+if [[ ${os_name} == Ubuntu* ]]; then
+    sudo apt install libssl-dev python3-tk libreadline-dev libsqlite3-dev tk-dev
+elif [[ ${os_name} == Manjaro* ]]; then
+    sudo pacman -S tk
+fi
