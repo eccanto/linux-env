d65474da758e5d5afb31c558d541f446439dd432
eccanto <ccanto.erik@gmail.com>
2024-05-05 13:36:43 -0300

    fix(scripts): remove hardcoded home user

--- scripts/screenlayouts/home.sh
+++ scripts/screenlayouts/home.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-1 --off --output HDMI-2 --primary --mode 2560x1080 --pos 1080x330 --rotate normal
-feh --bg-fill /home/eccanto/.wallpaper.jpg
+feh --bg-fill ~/.wallpaper.jpg
 openrgb --profile orange
--- scripts/screenlayouts/single.sh
+++ scripts/screenlayouts/single.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 xrandr --output DP-1 --off --output HDMI-1 --off --output HDMI-2 --mode 2560x1080 --pos 0x0 --rotate normal
-feh --bg-fill /home/eccanto/.wallpaper.jpg
+feh --bg-fill ~/.wallpaper.jpg
 openrgb --profile off
